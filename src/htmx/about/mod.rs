50c377070f33be06d4f63b84ae62ecc3831cba6e
Lorenz Mielke <info@lommix.de>
2024-05-19 11:59:59 +0200

    some style changes

--- src/htmx/about/mod.rs
+++ src/htmx/about/mod.rs
@@ -20,13 +20,15 @@ impl HtmxComponent<AppState> for AboutContent {
     fn handle() -> MethodRouter<AppState> {
         get(|| async move {
             html!{
-                h1{"About"}
-                hr {}
-                div class="about-content" {
-                    img src="/static/images/mcu.jpg"{}
-                    p{"My name is Lorenz, a web/game developer from Germany with a passion for exploring and learning new things. I am driven by curiosity and a desire to continuously learn and explore new ideas, which has led me on a journey from hardware development to embedded systems and eventually to web development and gaming. My interests lie in the field of system design, experimentation, and improving upon existing technologies through constant exploration and seeking out new ways of doing things."}
+                div class="about"{
+                    h1{"About"}
+                    hr {}
+                    div class="about-content" {
+                        img src="/static/images/mcu.jpg"{}
+                        p{"My name is Lorenz, a web/game developer from Germany with a passion for exploring and learning new things. I am driven by curiosity and a desire to continuously learn and explore new ideas, which has led me on a journey from hardware development to embedded systems and eventually to web development and gaming. My interests lie in the field of system design, experimentation, and improving upon existing technologies through constant exploration and seeking out new ways of doing things."}
+                    }
+                    p{"Dram Crystal Display Driver, AVR-Programming adapter"}
                 }
-                p{"Dram Crystal Display Driver, AVR-Programming adapter"}
             }.into_response()
         })
     }
